eff758a2f37389110679d6c08ce8607962dc7763
Anand A. Joshi <ajoshi@usc.edu>
2024-08-21 14:37:30 -0700

    updated the PET processing code

--- create_roi_mask.m
+++ create_roi_mask.m
@@ -1,17 +1,19 @@
-function create_roi_mask(labeled_image_path, roi_list, output_mask_path)
+function create_roi_mask(labeled_image_path, roi_list,pvc_label_path, output_mask_path)
     % Read the labeled brain image
     labeled_image = niftiread(labeled_image_path);
-    nii_info = niftiinfo(labeled_image_path);
-    
+    nii_info = niftiinfo(labeled_image_path);    
     % Initialize the mask
     mask = zeros(size(labeled_image));
-    
+
+    pvc_label= niftiread(pvc_label_path);
+
+    % pvc_label == 2 is Gray Matter
     % Iterate over the list of ROIs and create the mask
     for i = 1:length(roi_list)
         roi_value = roi_list(i);
-        mask(labeled_image == roi_value) = 1;
+        mask(labeled_image == roi_value & pvc_label == 2) = 1;
     end
-    
+     
     % Save the mask as a NIfTI file
     nii_info.Datatype='uint8';
     niftiwrite(uint8(mask), output_mask_path, nii_info);
--- main_pet_process.m
+++ main_pet_process.m
@@ -3,14 +3,34 @@ clc;clear all;close all;restoredefaultpath;
 addpath('/home/ajoshi/Projects/svreg/src');
 addpath('/home/ajoshi/Projects/svreg/3rdParty');
 
+
+
+% Motion correct and Average PET file
+raw_pet = '/deneb_disk/PETBrainStorm/test_0001/pet_brainsuite/trc-18FNAV4694_pet.nii.gz';
+system(['mcflirt -in ',raw_pet]); % this is motion correction method from FSL
+
+raw_pet_mc = [raw_pet(1:end-7),'_mcf.nii.gz'];
+
+ave_raw_pet = '/deneb_disk/PETBrainStorm/test_0001/pet_brainsuite/trc-18FNAV4694_pet_ave.nii.gz';
+
+p = niftiread(raw_pet_mc);
+info = niftiinfo(raw_pet);
+pave = mean(p,4);
+niftiwrite(p,ave_raw_pet,info)
+
+%% Before running this step, coregister PET (ave_raw_pet) to MRI using rigid registration
 % PET image coregistered to MRI
-pet2mri_file = '/home/ajoshi/Projects/PETBrainSuite/test_data/pet2mri2_itksnap.nii.gz'; 
+pet2mri_file = '/deneb_disk/PETBrainStorm/test_0001/pet_brainsuite/trc-18FNAV4694_pet_ave_reg.nii.gz'; 
 
 % PSF of PET image (from manufacturer)
 xres = '6.0'; yres = '6.0'; zres = '6.0';
 
+% vol smoothing FWHM
+vol_smooth_fwhm=5.0;
+vstd=vol_smooth_fwhm/2.355;
+
 % MRI image should be processed using BrainSuite upto SVReg
-mri_basefile = '/home/ajoshi/Projects/PETBrainSuite/test_data/mri';
+mri_basefile = '/deneb_disk/PETBrainStorm/test_0001/pet_brainsuite/T1w';
 PETPVC_EXE = '/home/ajoshi/Projects/PETBrainSuite/PETPVC-1.2.10/bin/petpvc';
 
 REF_ROIS = [900]% List of ROIS from atlas used as reference
@@ -19,11 +39,12 @@ REF_ROIS = [900]% List of ROIS from atlas used as reference
 
 
 % Output filenames
-pvc_mask_file = '/home/ajoshi/Projects/PETBrainSuite/test_data/pet2mri2_itksnap_mask.nii.gz';
-pet2mri_pvc_file = '/home/ajoshi/Projects/PETBrainSuite/test_data/pet2mri2_itksnap_pvc.nii.gz'; 
-pet2surf_file = '/home/ajoshi/Projects/PETBrainSuite/test_data/pet2mri2_itksnap_pvc.mat';
-pet_suvr_file = [mri_basefile,'.pet.suvr.nii.gz'];
-pet_ref_mask_file = [mri_basefile,'.pet.ref.mask.nii.gz'];
+pvc_mask_file = [pet2mri_file(1:end-7),'_mask.nii.gz'];
+pet2mri_pvc_file = [pet2mri_file(1:end-7),'_pvc.nii.gz']; 
+pet2mri_pvc_sm_file = pet2mri_pvc_file;
+pet2surf_file = [pet2mri_file(1:end-7),'.pvc.mat'];
+pet_suvr_file = [pet2mri_file(1:end-7),'.pet.suvr.nii.gz'];
+pet_ref_mask_file = [pet2mri_file(1:end-7),'.pet.ref.mask.nii.gz'];
 
 
 
@@ -61,13 +82,18 @@ cmd = [PETPVC_EXE,' -i ',pet2mri_file,' -m ',pvc_mask_file,' -o ',pet2mri_pvc_fi
 system(cmd);
 
 
+pvc_label_file = [mri_basefile,'.pvc.label.nii.gz'];
 
 % Create mask file for reference rois
-create_roi_mask([mri_basefile,'.svreg.label.nii.gz'], REF_ROIS, [mri_basefile,'.pet.ref.mask.nii.gz'])
+create_roi_mask([mri_basefile,'.svreg.label.nii.gz'], REF_ROIS, pvc_label_file, pet_ref_mask_file)
+
+
+% Volumetric smoothing of PVC corrected PET
+svreg_smooth_vol_function(pet2mri_pvc_file,vstd,vstd,vstd,pet2mri_pvc_sm_file)
+
 
 % Calculate SUVR
 calculate_voxelwise_suvr(pet2mri_pvc_file, pet_ref_mask_file,  pet_suvr_file)
-
 
 
 
@@ -76,8 +102,16 @@ resample2surf(mri_basefile,pet_suvr_file,pet2surf_file, 0)
 
 a = load(pet2surf_file);
 
+% do 5mm smoothing on the surface
+surf_std_dev = 5/2.355;
+
 left_surf = readdfs([mri_basefile, '.left.mid.cortex.svreg.dfs']);
 right_surf = readdfs([mri_basefile, '.right.mid.cortex.svreg.dfs']);
+
+a.datal = smooth_surf_function(left_surf,a.datal,surf_std_dev,surf_std_dev);
+a.datar = smooth_surf_function(right_surf,a.datar,surf_std_dev,surf_std_dev);
+
+
 
 figure;
 patch('faces',left_surf.faces,'Vertices',left_surf.vertices,'facevertexcdata',a.datal,'edgecolor','none','facecolor','interp');
@@ -88,6 +122,7 @@ figure;
 patch('faces',right_surf.faces,'Vertices',right_surf.vertices,'facevertexcdata',a.datar,'edgecolor','none','facecolor','interp');
 material dull;axis off;axis equal;axis tight;colormap('hot');view(90,0);clim([0,3]);
 camlight;
+
 
 left_surf_sm = smooth_cortex_fast(left_surf,.1,3000);
 right_surf_sm = smooth_cortex_fast(right_surf,.1,3000);
@@ -111,6 +146,10 @@ camlight;
 left_atlas_surf = readdfs([subdir, '/atlas.left.mid.cortex.svreg.dfs']);
 right_atlas_surf = readdfs([subdir, '/atlas.right.mid.cortex.svreg.dfs']);
 
+
+% do 5mm smoothing on the atlas surface
+a.datal_atlas = smooth_surf_function(left_atlas_surf,a.datal_atlas,surf_std_dev,surf_std_dev);
+a.datar_atlas = smooth_surf_function(right_atlas_surf,a.datar_atlas,surf_std_dev,surf_std_dev);
 
 figure;
 patch('faces',left_atlas_surf.faces,'Vertices',left_atlas_surf.vertices,'facevertexcdata',a.datal_atlas,'edgecolor','none','facecolor','interp');
